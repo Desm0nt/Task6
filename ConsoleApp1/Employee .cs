eca5968ee8f5bbd02b1d5a6349a1383689caeb33
Konstantin Panarin <logran2@gmail.com>
2018-02-10 17:31:51 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Employee .cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Employee : IComparable<Employee>
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Position { get; set; }
+        public int Cabinet { get; set; }
+        public DateTime HiringDate { get; set; }
+        public int Age { get; set; }
+        public int Salary { get; set; }
+
+        public int CompareTo(Employee obj)
+        {
+            return Salary.CompareTo(obj.Salary);
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine("Name: " + FirstName + " " + LastName 
+                + ", Position: " + Position 
+                + ", Cabinet: " + Cabinet 
+                + ", Hiring Date: " + HiringDate.ToString("dd/MM/yyyy", CultureInfo.InvariantCulture) 
+                + ", " + string.Format("Salary: {0:C2}", Salary, CultureInfo.InvariantCulture));
+        }
+
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> Workers = new List<Employee>();
+            for (int i = 0; i < 6; i++)
+            {
+                Workers.Add(new Employee() { FirstName = RandomString(7),
+                    LastName = RandomString(5),
+                    Position = RandomString(5),
+                    Age = random.Next(18, 55),
+                    Cabinet = random.Next(1, 255),
+                    HiringDate = Convert.ToDateTime(random.Next(1, 12) + "/" + random.Next(1, 12) + "/" + random.Next(1988, 2017)),
+                    Salary = random.Next(100, 1100) });
+            }
+            foreach(var worker in Workers)
+            {
+                worker.PrintInfo();
+            }
+            Workers.Sort();
+            Console.WriteLine("\n SORTED BY SALARY: \n");
+            foreach (var worker in Workers)
+            {
+                worker.PrintInfo();
+            }
+            List<Employee> firstNameWorkSort = Workers.OrderBy(o => o.FirstName).ToList();
+            Console.WriteLine("\n SORTED BY FIRST NAME: \n");
+            foreach (var worker in firstNameWorkSort)
+            {
+                worker.PrintInfo();
+            }
+            List<Employee> cabinetNameWorkSort = Workers.OrderBy(o => o.Cabinet).ToList();
+            Console.WriteLine("\n SORTED BY CABINET NUMBER: \n");
+            foreach (var worker in cabinetNameWorkSort)
+            {
+                worker.PrintInfo();
+            }
+            Console.Read();
+        }
+
+
+        private static Random random = new Random();
+        public static string RandomString(int length)
+        {
+            const string chars = "abcdefghijklmnopqrstuvwyxz";
+            return new string(Enumerable.Repeat(chars, length)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+    }
+}
